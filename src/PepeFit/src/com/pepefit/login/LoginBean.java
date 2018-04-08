9ce2a372f9b2eb5450bda0d28604d972ece65c3f
Bahadır Adak <bahadiradak95@gmail.com>
2018-04-08 09:46:47 +0300

    added redirect() method to LoginBean and changed action Log in button.

--- src/PepeFit/src/com/pepefit/login/LoginBean.java
+++ src/PepeFit/src/com/pepefit/login/LoginBean.java
@@ -6,7 +6,7 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class LoginBean {
 	
-	String username,password;
+	String username,password,accountType;
 
 	public String getUsername() {
 		return username;
@@ -23,5 +23,17 @@ public class LoginBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getAccountType() { /* DB'den gelen account type a göre sayfa yönlendirmesi yapmasý lazým */
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
 	
+	public String redirect() { /* bu kýsýma sonra if statements eklenerek accountType'a göre yönlendirme yapýlacak */
+		
+		return "admin/admin.xhtml?faces-redirect=true";
+	}
 }
