2a98353270aa5fdcd1b213bdd61aefc20a935843
Bahadır Adak <bahadiradak95@gmail.com>
2018-04-08 11:00:39 +0300

    adminPanelBean and admin operations xhtml pages added.

---
+++ src/PepeFit/src/com/pepefit/admin/AdminPanelBean.java
@@ -0,0 +1,100 @@
+package com.pepefit.admin;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class AdminPanelBean {
+
+	String memberName, memberTC, memberGender, memberEmail, memberAddress, memberPhone;
+	String trainerName, trainerGender, trainerPhone, trainerEmail, trainerDateOfBirth;
+	
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getMemberTC() {
+		return memberTC;
+	}
+	public void setMemberTC(String memberTC) {
+		this.memberTC = memberTC;
+	}
+	public String getMemberGender() {
+		return memberGender;
+	}
+	public void setMemberGender(String memberGender) {
+		this.memberGender = memberGender;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberAddress() {
+		return memberAddress;
+	}
+	public void setMemberAddress(String memberAddress) {
+		this.memberAddress = memberAddress;
+	}
+	public String getMemberPhone() {
+		return memberPhone;
+	}
+	public void setMemberPhone(String memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+	public String getTrainerName() {
+		return trainerName;
+	}
+	public void setTrainerName(String trainerName) {
+		this.trainerName = trainerName;
+	}
+	public String getTrainerGender() {
+		return trainerGender;
+	}
+	public void setTrainerGender(String trainerGender) {
+		this.trainerGender = trainerGender;
+	}
+	public String getTrainerPhone() {
+		return trainerPhone;
+	}
+	public void setTrainerPhone(String trainerPhone) {
+		this.trainerPhone = trainerPhone;
+	}
+	public String getTrainerEmail() {
+		return trainerEmail;
+	}
+	public void setTrainerEmail(String trainerEmail) {
+		this.trainerEmail = trainerEmail;
+	}
+	public String getTrainerDateOfBirth() {
+		return trainerDateOfBirth;
+	}
+	public void setTrainerDateOfBirth(String trainerDateOfBirth) {
+		this.trainerDateOfBirth = trainerDateOfBirth;
+	}
+	public String redirectAddMember() {
+		return "addMember.xhtml?faces-redirect=true";
+	}
+	public String redirectDeleteMember() {
+		return "deleteMember.xhtml?faces-redirect=true";
+	}
+	public String redirectUpdateMember() {
+		return "updateMember.xhtml?faces-redirect=true";
+	}
+	public String redirectAddTrainer() {
+		return "addTrainer.xhtml?faces-redirect=true";
+	}
+	public String redirectDeleteTrainer() {
+		return "deleteTrainer.xhtml?faces-redirect=true";
+	}
+	public String redirectUpdateTrainer() {
+		return "updateTrainer.xhtml?faces-redirect=true";
+	}
+	public String redirectIndexPage() {
+		return "index.xhtml?faces-redirect=true";
+	}
+}
