e1859575aee15c0d3486c266d2137b1ddc9d42e7
b21527285 <b21527285@cs.hacettepe.edu.tr>
2018-05-01 20:49:11 +0300

    merged script files, updated courses_trainer to support ege and eyupcan's changes

--- src/PepeFit/WebContent/resources/js/scripts.js
+++ src/PepeFit/WebContent/resources/js/scripts.js
@@ -37,7 +37,15 @@ function changeContainer(clickedOn) {
     document.getElementById(clickedOn).style.display = "contents";
     if (clickedOn )
     document.getElementById(clickedOn.concat("List")).classList.add("active");
+    resetForms();
     console.log(clickedOn);
+}
+
+function resetForms(){ 
+    var formList = ["addMember", "updateMember1", "updateMember2", "addTrainer", "updateTrainer1", "updateTrainer2", "delete"]
+    for (var i = 0; i<formList.length; i++){
+        document.getElementById(formList[i].concat("Form")).reset();
+    }
 }
 
 function updateSearch(idUpdate) {
@@ -68,6 +76,38 @@ function toggleTrainer(clickedOn) {
             lis[i].firstChild.className = lis[i].firstChild.className.concat(" active");
             document.getElementById(trainerpid).style.display = "block";
             console.log(document.getElementById(trainerpid));
+        }
+    }
+}
+
+function changeContainerCourses(clickedOn) {
+    var containerArrayCourses = ["addCourse", "updateCourse", "deleteCourse"];
+    for (var i = 0; i< containerArrayCourses.length; i++){
+        document.getElementById(containerArrayCourses[i].concat("List")).classList.remove("active");
+        document.getElementById(containerArrayCourses[i]).style.display = "none";
+    }
+    document.getElementById(clickedOn).style.display = "contents";
+    if (clickedOn )
+    document.getElementById(clickedOn.concat("List")).classList.add("active");
+    console.log(clickedOn);
+}
+
+function toggleCourse(clickedOn) {
+    var lis = document.getElementById("courses").getElementsByTagName("li");
+    for (var i=0; i<lis.length; i++){
+
+        lis[i].firstChild.classList.remove("active");
+        var coursedivid = lis[i].firstChild.id;
+        var coursepid = coursedivid.concat("p");
+        document.getElementById(coursepid).style.display = "none";
+        if (coursedivid==clickedOn){
+            lis[i].firstChild.className = lis[i].firstChild.className.concat(" active");
+            document.getElementById(coursepid).style.display = "block";
+            //document.getElementById("course-form").reset();
+            document.getElementById("block-func").textContent = coursedivid;
+            console.log(document.getElementById(coursepid));
+            document.getElementById("empty-container").style.display ="none";
+            document.getElementById("addCourse").style.display ="contents";
         }
     }
 }
