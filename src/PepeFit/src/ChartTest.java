bac0f881b2047962869781079c31add76103f829
Ege U <egeucak75@hotmail.com>
2018-05-14 17:58:57 +0300

    Now charts change with ajax. But changing charts require different methods. There needs to be one method which takes chart name as argument

--- src/PepeFit/src/ChartTest.java
+++ src/PepeFit/src/ChartTest.java
@@ -1,5 +1,7 @@
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import javax.faces.bean.ManagedBean;
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
@@ -11,53 +13,78 @@ import org.primefaces.model.*;
 @ManagedBean
 public class ChartTest implements Serializable {
 
+    private LineChartModel lineModel;
     private LineChartModel lineModel1;
+    private LineChartModel lineModel2;
+
+    private HashMap values = new HashMap();
 
     @PostConstruct
     public void init() {
         createLineModels();
     }
 
+    public LineChartModel getLineModel() { return lineModel; }
     public LineChartModel getLineModel1() {
-        return lineModel1;
+        lineModel = lineModel1;
+        return lineModel;
+    }
+    public LineChartModel getLineModel2() {
+        lineModel = lineModel2;
+        return lineModel;
     }
 
     private void createLineModels() {
         lineModel1 = initLinearModel();
-        lineModel1.setTitle("Progress");
+        lineModel1.setTitle("Arm");
         lineModel1.setLegendPosition("sw");
-        Axis yAxis = lineModel1.getAxis(AxisType.Y);
-        yAxis.setMin(0);
-        yAxis.setMax(10);
+        Axis yAxis1 = lineModel1.getAxis(AxisType.Y);
+        yAxis1.setMin(0);
+        yAxis1.setMax(10);
+
+        lineModel2 = initLinearModel2();
+        lineModel2.setTitle("Leg");
+        lineModel2.setLegendPosition("sw");
+        Axis yAxis2 = lineModel1.getAxis(AxisType.Y);
+        yAxis2.setMin(0);
+        yAxis2.setMax(10);
+
+        lineModel = lineModel1;
     }
 
     private LineChartModel initLinearModel() {
-        LineChartModel model = new LineChartModel();
 
+        LineChartModel model = new LineChartModel();
         LineChartSeries series1 = new LineChartSeries();
         series1.setLabel("Arm");
-
         series1.set(1, 2);
         series1.set(2, 1);
         series1.set(3, 3);
         series1.set(4, 6);
         series1.set(5, 8);
-
-        LineChartSeries series2 = new LineChartSeries();
-        series2.setLabel("Leg");
-
-        series2.set(1, 6);
-        series2.set(2, 3);
-        series2.set(3, 2);
-        series2.set(4, 7);
-        series2.set(5, 9);
-
-        model.setLegendRows(1);
-        model.setExtender("ext");
         model.addSeries(series1);
-        model.addSeries(series2);
+        model.setExtender("ext");
         model.setSeriesColors("ffffff");
         model.setShadow(false);
+        System.out.println("1");
+        return model;
+    }
+
+    private LineChartModel initLinearModel2() {
+
+        LineChartModel model = new LineChartModel();
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Arm");
+        series1.set(1, 5);
+        series1.set(2, 2);
+        series1.set(3, 7);
+        series1.set(4, 13);
+        series1.set(5, 6);
+        model.addSeries(series1);
+        model.setExtender("ext");
+        model.setSeriesColors("ffffff");
+        model.setShadow(false);
+        System.out.println("2");
         return model;
     }
 
